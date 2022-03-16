94a80ef02704ab8e27df83837082261268cf676d
mons1220 <mons1220@gmail.com>
2022-03-17 00:29:48 +0900

    4.13 Conclusions - Complete

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,24 +6,26 @@ const Home: NextPage = () => {
     grid gap-10 lg:grid-cols-2 xl:grid-cols-3 
     min-h-screen xl:place-content-center">
       
-      <div className="bg-white flex flex-col justify-between p-6 rounded-3xl shadow-xl">
-        <span className='font-semibold text-3xl'>Select Item</span>
+      <div className="bg-white dark:bg-black flex flex-col justify-between p-6 rounded-3xl shadow-xl">
+        <span className='font-semibold dark:text-white text-3xl'>Select Item</span>
         <ul>
           {[1, 2, 3, 4, 5].map((i) => (
             <li className='flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-500'>
-              <span className='text-gray-500'>Grey Chair</span>
-              <span className='font-semibold'>$19</span>
+              <span className='text-gray-500 dark:text-gray-100'>Grey Chair</span>
+              <span className='font-semibold dark:text-white'>$19</span>
             </li>
           ))}
         </ul>
         <div className='flex justify-between mt-2 pt-2 border-t-2 border-dashed'>
           <span>Total</span>
-          <span className='font-semibold'>$10</span>
+          <span className='font-semibold dark:text-white'>$10</span>
         </div>
         <button className='mt-5 bg-blue-500 text-white 
         p-3 text-center rounded-xl w-2/4 mx-auto block
         hover:bg-teal-500 hover:text-black
         active:bg-yellow-500 focus:bg-red-500
+        dark:bg-black dark:border-white dark:border
+        dark:hover:bg-white
         '>
           Checkout
         </button>
@@ -165,6 +167,11 @@ const Home: NextPage = () => {
         </div>
       </div>
 
+      <div className='bg-white p-5 rounded-3xl shadow-xl'>
+        <div className='flex flex-col space-y-2 p-5'>
+          <h2 className='text-[100px] text-[#cf4848] bg-[url("/vercel.svg")]'> 100px </h2>
+        </div>
+      </div>
     
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,6 @@ module.exports = {
   theme: {
     extend: {},
   },
+  darkMode: "media", //class <-이거는 부모 className에 dark를 넣어주면 활성화됨 (_app.tsx component를 div로 감싸고 className에 dark넣어주면 모든 게 다 적용되니까 가장 편한 방법)
   plugins: [],
 };
